31e64df87e19e83521c1e244d689955a153d2803
Stuart <stuy.bradley@gmail.com>
2019-12-21 20:43:07 +0000

    paths

--- tests/test_6.py
+++ tests/test_6.py
@@ -6,7 +6,7 @@ from challenge_6 import count_simple_paths, shortest_path
 @pytest.mark.parametrize(
     'file_path, path_count',
     [
-        ('/Users/stuart/PycharmProjects/AoC/tests/data/challenge_6_edge_list.txt', 42),
+        ('data\challenge_6_edge_list.txt', 42),
     ]
 )
 def test_count_paths(file_path, path_count):
@@ -16,7 +16,7 @@ def test_count_paths(file_path, path_count):
 @pytest.mark.parametrize(
     'file_path, path_count',
     [
-        ('/Users/stuart/PycharmProjects/AoC/tests/data/challenge_6_edge_list_2.txt', 4),
+        ('data\challenge_6_edge_list_2.txt', 4),
     ]
 )
 def test_shortest_path(file_path, path_count):
