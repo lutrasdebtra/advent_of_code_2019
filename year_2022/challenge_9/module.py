269c54d3ac768f6a7d4dbfec162eef06721909a6
Stuart Bradley <stuy.bradley@gmail.com>
2022-12-09 13:55:55 +0000

    P2 solution

--- year_2022/challenge_9/module.py
+++ year_2022/challenge_9/module.py
@@ -1,82 +1,39 @@
 from typing import List, Tuple, Text, Optional
-from collections import deque
 
+sign = lambda x: 1 if x > 0 else (-1 if x < 0 else 0)
 
-def make_grid(items):
-    return [[" " for _ in range(0, 100)] for _ in range(0, 100)]
-
-
-def place_on_grid(grid, x, y, c="#"):
-    grid[x][y] = c
-
-
-def repr_grid(grid):
-    return "\n".join("|" + "|".join(sub + [""]) for sub in grid)
-
-
-def place_items_on_grid(data, head):
-    grid = make_grid(data)
-    for x, y in data:
-        place_on_grid(grid, x, y)
-        place_on_grid(grid, 50, 50, c="S")
-        place_on_grid(grid, head[0], head[1], c="H")
-    print(repr_grid(grid))
-
-
-####
-
-
-def _move(direction: Text, pos: List[int]) -> List[int]:
-    if direction == "R":
-        return [pos[0] + 1, pos[1]]
-    elif direction == "L":
-        return [pos[0] - 1, pos[1]]
-    elif direction == "U":
-        return [pos[0], pos[1] + 1]
-    elif direction == "D":
-        return [pos[0], pos[1] - 1]
-
-
-DIRECTION_MAP = {"R": (0, 1), "L": (0, -1), "U": (1, 1), "D": (1, -1)}
-
-
-def _tail_in_range(head: List[int], tail: List[int]) -> bool:
-    return head in [
-        [tail[0], tail[1]],
-        [tail[0] + 1, tail[1]],
-        [tail[0] - 1, tail[1]],
-        [tail[0], tail[1] + 1],
-        [tail[0], tail[1] - 1],
-        [tail[0] + 1, tail[1] + 1],
-        [tail[0] - 1, tail[1] - 1],
-        [tail[0] + 1, tail[1] - 1],
-        [tail[0] - 1, tail[1] + 1],
-    ]
+DIRECTION_MAP = {"U": (0, 1), "D": (0, -1), "R": (1, 0), "L": (-1, 0)}
 
 
 def rope_positions(
     moves: List[Tuple[Text, int]], tail_length: Optional[int] = 1
 ) -> int:
-    print()
+    """
+    Partial solution taken from: https://github.com/nthistle/advent-of-code/blob/master/2022/day09/day09_p2.py
+    """
     tail_positions = set()
     snake_size = tail_length + 1
     snake = [[50, 50] for _ in range(snake_size)]
     tail_positions.add(tuple(snake[-1]))
-    snake_directions = deque([""] * snake_size, maxlen=snake_size)
     for direction, distance in moves:
-        print(direction, distance)
-        for g in range(distance):
-            snake_directions.appendleft(direction)
-            print(direction, g, snake_directions)
-            for i in range(len(snake)):
-                if i == 0:
-                    snake[i] = _move(direction, snake[i])
+        for _ in range(distance):
+            delta_x, delta_y = DIRECTION_MAP[direction]
+            snake[0][0] += delta_x
+            snake[0][1] += delta_y
+            for i in range(1, len(snake)):
+                head_x, head_y = snake[i - 1]
+                tail_x, tail_y = snake[i]
+                delta_x = tail_x - head_x
+                delta_y = tail_y - head_y
+                if (abs(delta_x), abs(delta_y)) == (1, 1):
                     continue
-                elif not _tail_in_range(snake[i - 1], snake[i]):
-                    print(i, snake[i - 1], snake[i], snake_directions[i - 1])
-                    snake[i] = snake[i - 1].copy()
-                    coord_position, coord_amount = DIRECTION_MAP[direction]
-                    snake[i][coord_position] -= coord_amount
+                elif delta_x == 0 or delta_y == 0:
+                    if abs(delta_x) >= 2:
+                        snake[i][0] -= sign(delta_x)
+                    if abs(delta_y) >= 2:
+                        snake[i][1] -= sign(delta_y)
+                else:
+                    snake[i][0] -= sign(delta_x)
+                    snake[i][1] -= sign(delta_y)
             tail_positions.add(tuple(snake[-1]))
-        place_items_on_grid(snake[:-1], snake[-1])
     return len(tail_positions)
